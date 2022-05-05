029f6aeba3b6c8429da48d99f85f09fa693bf929
hunterll2 <50384403+hunterll2@users.noreply.github.com>
2022-05-05 11:52:41 +0300

    rewrite the preprocess

--- TopicModelingSearchEngine/preprocess.py
+++ TopicModelingSearchEngine/preprocess.py
@@ -1,28 +1,35 @@
 import re
 import spacy
-nlp = spacy.load("en_core_web_sm")
 import pickle
 import constants as c
+nlp = spacy.load("en_core_web_sm")
 
 # Module functions
 contractions_re = re.compile('(%s)' % '|'.join(c.CONTRACTIONS_DICT.keys()))
+
 def expand_contractions(text, contractions_dict = c.CONTRACTIONS_DICT):
     def replace(match):
         return contractions_dict[match.group(0)]
     return contractions_re.sub(replace, text)
 
-def clean_text(text):
-    text = re.sub('\w*\d\w*','', text)
-    text = re.sub('\n',' ',text)
-    text = re.sub(r"http\s+", "", text)
-    text = re.sub('[^a-z]',' ',text)
-    return text
-
 def clean(txt):
     cleand = []
+
     txt = txt.lower()
-    txt = clean_text(txt)
+
+    # convert contractions into its original form
+    txt = expand_contractions(txt)
+
+    # remove digits
+    txt = re.sub('\w*\d\w*', '', txt)
+
+    # remove empty lins
+    txt = re.sub('\n', ' ', txt)
+
+    # remove brackets, links, random letters
+    txt = re.sub('[^a-z]', ' ', txt)
     
+    # normalize each word and exclude stops, spaces, and characters
     for token in list(nlp(txt)):
         clean_token = token.lemma_
             
@@ -32,21 +39,19 @@ def clean(txt):
 
         cleand.append(clean_token)
 
+    # convert the list of words into one string
     return ' '.join([str(e) for e in cleand])
 
 def create_new(df, num = 50000):
+    # take a portion of the dataset
     df = df.iloc[:num]
     df.columns=['docid','url','title','body']
     df = df.dropna()
 
-    df['cleaned']=df['body'].apply(lambda x: x.lower())
-    df['cleaned']=df['cleaned'].apply(lambda x: expand_contractions(x))
-    df['cleaned']=df['cleaned'].apply(lambda x: clean_text(x))
-    df['cleaned']=df['cleaned'].apply(lambda x: re.sub(' +',' ',x))
-    df['cleaned']=df['cleaned'].apply(lambda x: [token.lemma_ for token in list(nlp(x)) if token.is_stop==False])
-    df['cleaned']=df['cleaned'].apply(lambda x: [token for token in x if len(token)>=3])
-    df['cleaned']=df['cleaned'].apply(lambda x: ' '.join([str(e) for e in x]))
+    # for every doucment text perform the clean preprocess
+    df['cleaned'] = df['body'].apply(lambda x: clean(x))
 
+    # save the new cleaned dataset
     pickle.dump( df, open( "dataset/"+c.CLEANED_CORPUS_TABLE, "wb" ) )
 
     return
